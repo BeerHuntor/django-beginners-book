5e534c23766830156c91149d398ef095648bdc7d
HuntorGames <rt001a9138@gmail.com>
2024-05-22 16:19:39 +0100

    created blog post model

--- chapter-6-blog/blog/models.py
+++ chapter-6-blog/blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    blog_content = models.TextField()
+
+
+    def __str__(self):
+        return f'{self.title}, Writen by {self.author}, Created: {self.date_created}'
+    
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"pk": self.pk})
+    
