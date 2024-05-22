e9d614fac510cdc547694796c9ba5c61a94c8f46
HuntorGames <rt001a9138@gmail.com>
2024-05-22 14:08:26 +0100

    added tests and ran collectstatic

--- chapter-4-message-board-app/message_board_main/settings.py
+++ chapter-4-message-board-app/message_board_main/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -55,7 +55,7 @@ ROOT_URLCONF = "message_board_main.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [BASE_DIR / 'templates'],
+        "DIRS": [ BASE_DIR / 'templates'],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -117,6 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
+STATIC_DIRS = [
+    STATIC_ROOT,
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- chapter-4-message-board-app/posts/tests.py
+++ chapter-4-message-board-app/posts/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
-
+from django.urls import reverse
+from posts.models import Post
 # Create your tests here.
+
+class PostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.objects.create(post_content="This is a test!")
+
+    def test_model_content(self):
+        self.assertEqual(self.post.post_content, "This is a test!")
+
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+        self.assertContains(response, "This is a test!")
